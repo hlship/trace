4939f70447fc8c1fd9d2c65864198c1ee4caddd6
Howard Lewis Ship <h@lewisship.net>
2022-03-10 13:29:59 -0800

    Clean up build

--- build.clj
+++ build.clj
@@ -1,5 +1,6 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [net.lewisship.build :refer [requiring-invoke]]))
 
 (def lib 'io.github.hlship/trace)
 (def version "v0.1")
@@ -14,16 +15,16 @@
 
 (defn jar
   [_params]
-  ((requiring-resolve 'net.lewisship.build.jar/create-jar) jar-params))
+  (requiring-invoke net.lewisship.build.jar/create-jar jar-params))
 
 (defn deploy
   [_params]
   (clean nil)
   (jar nil)
-  ((requiring-resolve 'net.lewisship.build.jar/deploy-jar) jar-params))
+  (requiring-invoke net.lewisship.build.jar/deploy-jar jar-params))
 
 (defn codox
   [_params]
-  ((requiring-resolve 'net.lewisship.build.codox/generate)
-   {:project-name lib
-    :version version}))
+  (requiring-invoke net.lewisship.build.codox/generate
+                    {:project-name lib
+                     :version version}))
