25d86a56a588d48e0f5c57dcdd735f7e78da5829
Howard M. Lewis Ship <hlship@gmail.com>
2024-03-08 14:27:59 -0800

    Prep for 1.1 release

--- build.clj
+++ build.clj
@@ -1,30 +1,30 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [net.lewisship.build :refer [requiring-invoke]]))
+  (:require [net.lewisship.build :as b]
+            [clojure.tools.build.api :as build]))
 
 (def lib 'io.github.hlship/trace)
-(def version "v1.0")
+(def version "1.1")
 
 (def jar-params {:project-name lib
                  :version version
-                 :url "https://github.com/hlship/trace"})
+                 :scm
+                 {:url     "https://github.com/hlship/trace"
+                  :license :asl}})
 
 (defn clean
   [_params]
-  (b/delete {:path "target"}))
+  (build/delete {:path "target"}))
 
 (defn jar
   [_params]
-  (requiring-invoke net.lewisship.build.jar/create-jar jar-params))
+  (b/create-jar jar-params))
 
 (defn deploy
   [_params]
   (clean nil)
-  (jar nil)
-  (requiring-invoke net.lewisship.build.jar/deploy-jar jar-params))
+  (b/deploy-jar (assoc (jar nil) :sign-artifacts? false)))
 
 (defn codox
   [_params]
-  (requiring-invoke net.lewisship.build.codox/generate
-                    {:project-name lib
-                     :version version}))
+  (b/generate-codox {:project-name lib
+                     :version      version}))
